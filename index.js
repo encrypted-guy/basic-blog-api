dcbe776f9d952873c4bf7d771de4459a5b7ed7eb
encrypted_guy <nafiu.developer@gmail.com>
2021-07-31 23:11:22 +0500

    basic secure

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
+const rateLimit = require('express-rate-limit')
+const helmet = require('helmet')
+const xss = require('xss-clean')
+const hpp = require('hpp')
+
 const dotenv = require('dotenv')
 
 const app = express()
@@ -8,6 +13,15 @@ const app = express()
 // MIDDLEWIRES
 dotenv.config({ path: './config/config.env' })
 app.use(morgan('dev'))
+// --secure
+app.use(helmet())
+app.use(xss())
+app.use(hpp())
+const limiter = rateLimit({ 
+    windowMs: 10 *60 *1000,
+    max: 100
+})
+app.use(limiter)
 
 require('./config/db')
 app.use(express.json())
